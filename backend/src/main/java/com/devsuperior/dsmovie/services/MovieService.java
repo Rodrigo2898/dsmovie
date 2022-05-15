adb709ebb991dd66746261d94618da7859593780
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2022-05-14 22:38:30 -0300

    Find movies

---
+++ backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -0,0 +1,33 @@
+package com.devsuperior.dsmovie.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.Movie;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+
+@Service                    //Registra o MovieService como um componente do sistema
+public class MovieService {
+	
+	@Autowired
+	private MovieRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<MovieDTO> findAll(Pageable pageable) {
+		Page<Movie> result = repository.findAll(pageable); //"chamando" o banco de dados
+		Page<MovieDTO> page = result.map(x -> new MovieDTO(x));
+		return page;
+	}
+	
+	//Buscando apenas uma única página
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id) {
+		Movie result = repository.findById(id).get();
+		MovieDTO dto = new MovieDTO(result);
+		return dto;
+	}
+}
